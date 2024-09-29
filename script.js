cbbd847e7cbc3da23bb1452fcc511099cad2a8c8
samiullah <yarisamiullah@gmail.com>
2024-09-28 17:49:14 -0700

    tech-page-done

--- script.js
+++ script.js
@@ -109,3 +109,119 @@ desLinks.forEach((link) => {
 /* ========== */
 /* Crew */
 /* ========== */
+
+const crewBtns = document.querySelectorAll(".dot-indicators button");
+const crewImgCont = document.querySelector(".crew-img-cont");
+const crewRole = document.querySelector(".role");
+const crewName = document.querySelector(".crew-name");
+const textInfoJs = document.querySelector(".text-info-js");
+
+crewBtns.forEach( crew => {
+	crew.addEventListener("click", () => {
+		crewBtns.forEach((eachBtn) => {
+			eachBtn.setAttribute("aria-selected", "false")
+			eachBtn.classList.remove("dot-on")
+		})
+		console.log(crew.id)
+		handleCrew(crew)
+	})
+})
+
+function handleCrew(crew) {
+	if(crew.id === "commander") {
+		crewRole.innerHTML = planetData.crew[0].role;
+		crewName.innerHTML = planetData.crew[0].name;
+		textInfoJs.innerHTML = planetData.crew[0].bio;
+		crewImgCont.innerHTML = `<img src="${planetData.crew[0].images.png}" alt="Crew member image">`;
+		crew.classList.add("dot-on")
+		crew.setAttribute("aria-selected", "true")
+	}
+	else if(crew.id === "specialis") {
+		crewRole.innerHTML = planetData.crew[1].role;
+		crewName.innerHTML = planetData.crew[1].name;
+		textInfoJs.innerHTML = planetData.crew[1].bio;
+		crewImgCont.innerHTML = `<img src="${planetData.crew[1].images.png}" alt="Crew member image">`;
+		crew.classList.add("dot-on")
+		crew.setAttribute("aria-selected", "true")
+	}
+	else if(crew.id === "pilot") {
+		crewRole.innerHTML = planetData.crew[2].role;
+		crewName.innerHTML = planetData.crew[2].name;
+		textInfoJs.innerHTML = planetData.crew[2].bio;
+		crewImgCont.innerHTML = `<img src="${planetData.crew[2].images.png}" alt="Crew member image">`;
+		crew.classList.add("dot-on")
+		crew.setAttribute("aria-selected", "true")
+	}
+	else {
+		crewRole.innerHTML = planetData.crew[3].role;
+		crewName.innerHTML = planetData.crew[3].name;
+		textInfoJs.innerHTML = planetData.crew[3].bio;
+		crewImgCont.innerHTML = `<img src="${planetData.crew[3].images.png}" alt="Crew member image">`;
+		crew.classList.add("dot-on")
+		crew.setAttribute("aria-selected", "true")
+	}
+}
+
+/* ================= */
+/* technology styles */
+/* ================= */
+
+const techImgCont = document.querySelector(".tech-img-cont");
+const numberedDots = document.querySelectorAll(".numbered-dots .numbered-dot");
+const terminology = document.querySelector(".terminology ");
+const techName = document.querySelector(".tech-name");
+const techInfo = document.querySelector(".tech-info-js");
+let selectedTechNumb = 0;
+
+function handleTechSlides(btn) {
+	if(btn.id === "1"){
+		techName.innerHTML = planetData.technology[0].name;
+		techInfo.innerHTML = planetData.technology[0].description;
+		selectedTechNumb = 0
+		handleTechImgDeviceSize()
+	}
+	else if(btn.id === "2"){
+		techName.innerHTML = planetData.technology[1].name;
+		techInfo.innerHTML = planetData.technology[1].description;
+		selectedTechNumb = 1
+		handleTechImgDeviceSize()
+	}
+	else if(btn.id === "3"){
+		techName.innerHTML = planetData.technology[2].name;
+		techInfo.innerHTML = planetData.technology[2].description;
+		selectedTechNumb = 2
+		handleTechImgDeviceSize()
+	}
+}
+
+numberedDots.forEach(eachBtn =>{
+	eachBtn.addEventListener("click", () =>{
+		numberedDots.forEach(btn => {
+			btn.classList.remove("is-on")
+		})
+		handleTechSlides(eachBtn)
+		eachBtn.classList.add("is-on")
+		console.log(typeof eachBtn.id)
+	})
+})
+
+
+/* sets the corresponding technology image */
+function handleTechImgDeviceSize() {
+	if(window.innerWidth > 560 && window.innerWidth < 720) {
+		techImgCont.innerHTML = `
+		<img src="${planetData.technology[selectedTechNumb].images.landscape}" alt="technology image">`;
+	}
+	else {
+			techImgCont.innerHTML = `
+			<img src="${planetData.technology[selectedTechNumb].images.portrait}" alt="technology image">`;
+	}
+}
+
+setTimeout(() => {
+	handleTechImgDeviceSize()
+}, 500)
+
+window.addEventListener("resize", handleTechImgDeviceSize)
+/* if opended with tablet, sets the corresponding technology image  */
+handleTechImgDeviceSize()
